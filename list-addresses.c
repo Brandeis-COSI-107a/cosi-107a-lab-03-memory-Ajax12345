87ad4b58b6388854bccc951076d7bef8e9596426
james <james@kali.cosi107a.org>
2025-02-11 12:43:48 -0500

    updates

---
+++ list-addresses.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int var3;
+int add(int a, int b){
+    return a + b;
+}
+
+int main(int argc, char *argv[]) {
+    int var1 = 10;
+    char var2[] = "James Petullo";
+    int *ptr_var1 = &var1;
+    char *ptr_var2 = var2;
+    int *ptr_var3 = &var3;
+
+    printf("stack variable: 0x%012lx\n", ptr_var1);
+    printf("initialized data: %p\n", &var2);
+    printf("uninitialized data: 0x%012lx\n", ptr_var2);
+    printf("main: %p\n", &main);
+    printf("function: %p\n", &add);
+
+
+    return 0;
+}
